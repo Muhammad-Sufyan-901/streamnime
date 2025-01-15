c289d2e28af304fae1a193a8660ea752bb95920b
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2025-01-16 00:33:23 +0800

    feat : adding main layout

---
+++ app/views/layouts/main/partials/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer text-center text-white p-4">
+    <aside>
+        <p>Copyright © 2025 - StreamNime</p>
+    </aside>
+</footer>
---
+++ app/views/layouts/main/partials/navbar.php
@@ -0,0 +1,38 @@
+<header class="header" id="header">
+    <nav class="navbar container mx-auto px-0 py-6 flex justify-between items-center">
+        <div class="flex items-center gap-4">
+            <button class="navbar-toggle">
+                <i class='bx bx-menu'></i>
+            </button>
+
+            <img src="./assets/images/logo/logo-streamnime.png" alt="StreamNime Logo" width="175" class="logo">
+        </div>
+
+        <form
+            action=""
+            class="form form-container w-1/3"
+            method="POST"
+            id="login-form">
+            <div class="form-control">
+                <input
+                    type="search"
+                    name="search"
+                    id="search"
+                    required
+                    placeholder="Search Anime" />
+            </div>
+        </form>
+
+        <div class="flex items-center gap-4">
+            <a href=""
+                class="btn btn-primary btn-md">
+                Login <i class='bx bx-log-in'></i>
+            </a>
+
+            <a href=""
+                class="btn btn-primary-outline btn-md">
+                Register <i class='bx bx-log-in'></i>
+            </a>
+        </div>
+    </nav>
+</header>
