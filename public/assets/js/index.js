633e32718089f838c4771c819bd959cbf9be5869
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2025-01-16 00:33:44 +0800

    feat : adding on header scroll javascript function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const header = document.querySelector("header#header");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 75) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+});
